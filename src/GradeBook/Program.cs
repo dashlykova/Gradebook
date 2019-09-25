9094f481b69fc9d0858f1af2b768844feae6c8ed
Daria Lykova <daria.lykova@nepa.com>
2019-09-25 15:40:48 +0200

    Tries out a for loop

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var x = 34.1;
-            var y = 24.6;
-            var result = x + y;
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
             Console.WriteLine(result);
+
 
             if (args.Length > 0)
             {
