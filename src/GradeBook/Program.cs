11277c1a09a6231867fb975d62463cedfb9e6374
Daria Lykova <Daria.lykova@nepa.local>
2019-09-25 15:21:41 +0200

    Learns some simple variable declaration and creating floating numbers

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dash!");
+            var x = 34.1;
+            var y = 24.6;
+            var result = x + y;
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]} !");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
+
     }
 }
